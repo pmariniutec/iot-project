b8549925db60bc996f807d6e68da630f08af86c2
Piero Marini <piero.marini21@gmail.com>
2023-11-28 12:22:52 -0500

    feat: more robust debug

--- server/main.py
+++ server/main.py
@@ -1,32 +1,30 @@
 from yoloface.face_detector import YoloDetector
 import argparse
-import numpy as np
-from PIL import Image
 import cv2
 
 parser = argparse.ArgumentParser(prog='facecounter', description='count faces using yolo model')
 parser.add_argument('filename')
 args = parser.parse_args()
 
-model = YoloDetector(device="cuda:0", min_face=40)
-src = cv2.imread(args.filename)
-i = cv2.rotate(src, cv2.ROTATE_90_CLOCKWISE)
-# orgimg = Image.open(args.filename)
-# img = orgimg.rotate(-90)
-# imgarray = np.array(img)
+model = YoloDetector(device="cuda:0", min_face=20)
+image = cv2.imread(args.filename)
+# image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
 
-bboxes, points = model.predict(i)
-people_count = len(bboxes)
-print(people_count)
+bboxes, points = model.predict(image)
+print(bboxes)
 
 if (len(bboxes) and len(bboxes[0])):
-    bbox = bboxes[0][0]
-    start = (bbox[0], bbox[1])
-    end = (bbox[2], bbox[3])
-    print(start, end)
+    people_count = len(bboxes[0])
+    for bbox in bboxes[0]:
+        start = (bbox[0], bbox[1])
+        end = (bbox[2], bbox[3])
+        print(start, end)
+        # TMP: visualize predictions
+        image = cv2.rectangle(image, start, end, (0, 0, 255), 3)
+    print(f"People: {people_count}")
+else:
+    print("People: 0")
 
-    # TMP. visualize predictions
-    image = cv2.rectangle(i, start, end, (0, 0, 255), 5)
-    cv2.imshow("Rectangle", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+cv2.imshow("Rectangle", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
